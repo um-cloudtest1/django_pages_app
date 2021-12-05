d3821d4285626122668d2c0a8ec2f1ab0e3b3e26
um-cloudtest1 <cloudtest1@ftml.net>
2021-12-05 12:41:03 -0700

    Changed debug=False to debug=True

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '&h_@id5h+q#jc$2pr06_mqfy#h*h%+(9tbscyac@7-blfjv6np'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
